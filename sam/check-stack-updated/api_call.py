55fb6f79d61841f198a592624a075f5d34b2c2c1
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-09-24 10:11:36 +0100

    Updating action to fail if there is a rollback

--- sam/check-stack-updated/api_call.py
+++ sam/check-stack-updated/api_call.py
@@ -43,34 +43,37 @@ def wait_for_stack_status(stack_name, from_date, max_attempts=100):
     print(f"Waiting for stack {stack_name} to reach CREATE_COMPLETE, or UPDATE_COMPLETE status...")
     attempts = 0
     while attempts < max_attempts:
-        response = call_describe_stack_events(stack_name)
-        if not response:
-            print("No response received.")
-            break
+        try:
+            response = call_describe_stack_events(stack_name)
+            if not response:
+                print("No response received.")
+                break
 
-        events = response['StackEvents']
-        # print(events)
-        num_events = len(events)
+            events = response['StackEvents']
+            # print(events)
+            num_events = len(events)
 
-        for i in range(0, num_events):
-            event = events[i]
+            for i in range(0, num_events):
+                event = events[i]
 
-            update_complete, create_complete, update_failed = (
-                check_stack_status(event, from_date))
-            if update_complete:
-                print("Stack update complete.")
-                return
+                update_complete, create_complete, update_failed = (
+                    check_stack_status(event, from_date))
+                if update_complete:
+                    print("Stack update complete.")
+                    return
 
-            if create_complete:
-                print("Stack creation complete.")
-                return
-            if update_failed:
-                print("Stack update failed.")
-                return -1
-
-        attempts += 1
-        print(f"Attempt {attempts}/{max_attempts}: Waiting for stack to reach desired status...")
-        time.sleep(2)
+                if create_complete:
+                    print("Stack creation complete.")
+                    return
+                if update_failed:
+                    print("Stack update failed.")
+                    return -1
+        except ValidationError:
+            print("There was an ValidationError")
+        finally:
+            attempts += 1
+            print(f"Attempt {attempts}/{max_attempts}: Waiting for stack to reach desired status...")
+            time.sleep(2)
 
     print("Max attempts reached or desired status not found within the attempts limit.")
 
