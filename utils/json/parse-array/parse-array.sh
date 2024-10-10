68d24671a4a6ad95a11b36abc5dda1d923e862fb
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-10-10 14:33:44 +0100

    TEST parse JSON array - fix error

--- utils/json/parse-array/parse-array.sh
+++ utils/json/parse-array/parse-array.sh
@@ -2,9 +2,5 @@ set -eu
 
 : "${STRING}" # The string to parse into a JSON array
 
-tr '\n' ' ' <<< "$STRING" >> "$GITHUB_STEP_SUMMARY"
-
-exit 0
-
-read -ra values < <(tr '\n' ' ' <<< "$STRING")
+read -ra values <<< "$(tr '\n' ' ' <<< "$STRING")"
 jq --raw-input < <(IFS=$'\n' && echo "${values[*]}") | jq --slurp --compact-output
