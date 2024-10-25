7a33915f5c5506c86320aae6bcc9ef969d61f3f7
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-10-25 04:12:30 +0100

    OJ-2824: Add a script to list all buckets in a stack

---
+++ scripts/aws/cloudformation/list-stack-buckets.sh
@@ -0,0 +1,24 @@
+# Returns the names of all of the buckets in a stack
+
+set -eu
+base_dir="$(dirname "${BASH_SOURCE[0]}")"
+report="$base_dir"/../../report-step-result/print-list.sh
+
+: "${STACK_NAME}"          # Name of the stack containing the buckets to list
+: "${VERBOSE:=true}"       # Whether to print to step summary
+: "${ERROR_STATUS:=false}" # Whether to exit with an error status if no buckets have been found
+
+buckets=$(aws cloudformation list-stack-resources \
+  --stack-name "$STACK_NAME" \
+  --query "StackResourceSummaries[?ResourceType=='AWS::S3::Bucket'].PhysicalResourceId" \
+  --output text)
+
+if ! [[ $buckets ]]; then
+  $VERBOSE && echo "No buckets found in stack $STACK_NAME" >> "$GITHUB_STEP_SUMMARY"
+  $ERROR_STATUS && exit 1 || exit 0
+fi
+
+$VERBOSE && VALUES="$buckets" MESSAGE="Found buckets in stack \`$STACK_NAME\`" \
+  SINGLE_MESSAGE="Found a bucket in stack \`$STACK_NAME\`" $report >> "$GITHUB_STEP_SUMMARY"
+
+echo "$buckets"
