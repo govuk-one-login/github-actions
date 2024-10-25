7ee55dcb75db4be492a1f7bbf40473b27deda1d1
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-10-25 04:12:30 +0100

    OJ-2824: Delete object versions from a bucket
    Update the empty-bucket.sh script to list and delete all object versions and deletion markers from a bucket to empty it and allow CloudFormation to delete it

--- scripts/aws/s3/empty-bucket.sh
+++ scripts/aws/s3/empty-bucket.sh
@@ -1,11 +1,59 @@
-# Deletes all current objects from a bucket
+# Deletes all objects and object versions from a bucket
 set -eu
 
-: "${BUCKET}"        # Name of the bucket to empty
-: "${VERBOSE:=true}" # Whether to print to step summary
-
-echo "Deleting objects from bucket $BUCKET"
-aws s3 rm "s3://$BUCKET" --recursive --quiet
+: "${BUCKET}"         # Name of the bucket to empty
+: "${STEP:=1000}"     # Step size when removing object versions
+: "${VERBOSE:=false}" # Whether to print to step summary
 
 $VERBOSE && step_summary=$GITHUB_STEP_SUMMARY
-echo "Emptied bucket \`$BUCKET\`" | tee -a "${step_summary[@]}"
+
+function get-versioning {
+  aws s3api get-bucket-versioning --bucket "$BUCKET" --output text
+}
+
+function get-object-versions {
+  aws s3api list-object-versions --bucket "$BUCKET" --query \
+    "{Versions: Versions[].{Key:Key,VersionId:VersionId}, DeleteMarkers: DeleteMarkers[].{Key:Key,VersionId:VersionId}}"
+}
+
+function delete-objects {
+  local type=$1 objects count
+  objects=$(jq --compact-output ".$type" <<< "$versions")
+  count=$(jq length <<< "$objects")
+
+  [[ $count -eq 0 ]] && echo "  No $type found" && return 0
+  echo "  Deleting $count $type... "
+
+  local batch lower=0 upper=0
+  while [[ $upper -lt $count ]]; do
+    upper=$((upper + STEP))
+    [[ $upper -gt $count ]] && upper=$count
+    batch=$(jq --compact-output "{Objects: .[$lower:$upper], Quiet: true}" <<< "$objects")
+    aws s3api delete-objects --bucket "$BUCKET" --delete file:///dev/stdin <<< "$batch"
+    lower=$upper
+  done
+}
+
+function delete-versions {
+  delete-objects Versions && delete-objects DeleteMarkers
+}
+
+echo "Emptying bucket $BUCKET"
+
+echo "  Deleting objects..."
+aws s3 rm "s3://$BUCKET" --recursive --quiet
+
+echo -n "  Checking versioning configuration... "
+[[ $(get-versioning) == Enabled ]] && versioning=true && echo "[Enabled]" || echo "[Disabled]"
+
+if ${versioning:-false}; then
+  echo "  Getting object versions..."
+  versions=$(get-object-versions)
+
+  if ! delete-versions; then
+    echo "❌ Failed to empty bucket \`$BUCKET\`" | tee -a "${step_summary[@]}"
+    exit 1
+  fi
+fi
+
+echo "🗑️ Emptied bucket \`$BUCKET\`" | tee -a "${step_summary[@]}"
